dbcecc3b72ea57a796dc7c09c03b9a9503070afd
shubhi1928 <shubhijoshi28@gmail.com>
2022-02-12 00:03:17 +0530

    integrated metamask

--- src/components/Bottom_btn.vue
+++ src/components/Bottom_btn.vue
@@ -1,11 +1,53 @@
 <template>
     <div>
-        <button id="transfer">Transfer</button>
+
+        <button v-on:click="initWeb3" id="transfer">Transfer</button>
     </div>
 </template>
 <script>
+import Web3 from 'web3'
 export default {
-    name: "Bottom_btn"
+    name: "Bottom_btn",
+    data()
+    {
+        return {ether:{
+         networkId: null,
+         coinbase:null,
+         balance:null
+
+        }}
+    },
+    methods:{
+        
+async initWeb3 () {
+      // Check for web3 provider
+      if (typeof window.ethereum !== 'undefined') {
+        try {
+          // Ask to connect
+          await window.ethereum.send('eth_requestAccounts');
+          const instance = new Web3(window.ethereum)
+          // Get necessary info on your node
+          this.ether.networkId = await instance.eth.net.getId();
+         this.ether.coinbase = await instance.eth.getCoinbase();
+          this.ether.balance= await instance.eth.getBalance(this.ether.coinbase)
+          console.log(this.ether)
+          console.log(this.ether.networkId)
+          
+        } catch (error) {
+          // User denied account access
+          console.error('User denied web3 access', error);
+          return;
+        }        
+      }
+      // No web3 provider
+      else {
+        console.error('No web3 provider detected');
+        return;
+      }
+    }
+
+
+    }
 }
 </script>
 <style scoped>
